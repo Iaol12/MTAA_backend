240b03d3402e70adbc63cf8a44beaa6c0345a25f
ilaolioa <noidonthinkso@example.com>
2025-03-11 17:29:57 +0100

    pridane endpointy na testovanie

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,20 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+use App\Models\Item;
 
+Route::get('/items', function () {
+    return response()->json(Item::all());
+});
+
+Route::post('/items', function (Request $request) {
+    $request->validate([
+        'name' => 'required|string|max:255',
+    ]);
+    
+    $item = Item::create(['name' => $request->name]);
+    return response()->json($item, 201);
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
